6beea68bade29c00dd704f0598e47a327427340a
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-04-02 16:38:38 +0800

    First commit

--- BeautyOfProgramming/Program.cs
+++ BeautyOfProgramming/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace BeautyOfProgramming
 {
@@ -10,6 +12,28 @@ namespace BeautyOfProgramming
     {
         static void Main(string[] args)
         {
+            MakeUsage(75f);
+        }
+
+        private static void MakeUsage(float level)
+        {
+            PerformanceCounter p = new PerformanceCounter("Processor Information",
+                "% Processor Utility", "_Total");
+
+            int counter = 0;
+
+            while (true)
+            {
+                //Console.WriteLine(p.NextValue());
+                if (p.NextValue() > level)
+                {
+                    Thread.Sleep(10);
+                }
+                //else
+                //{
+                //    counter = (counter + 1) % Int32.MaxValue;
+                //}
+            }
         }
     }
 }
